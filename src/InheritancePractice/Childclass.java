6c3ffbe0e34c418b0c92fa1098f48afcc42e280a
rakesh <rakeshkanneboina@gmail.com>
2023-09-14 20:42:47 +0530

    commit

---
+++ src/InheritancePractice/Childclass.java
@@ -0,0 +1,28 @@
+package InheritancePractice;
+
+public class Childclass extends Parentclass{
+
+	public static void main(String[] args) {
+		 
+   cc();
+   rpm();
+   gear();
+   engine();
+   getcolor();
+		
+		
+	}
+	
+	public static void cc() {
+		System.out.println("cc implemented");
+	}
+	public static void rpm() {
+		
+		System.out.println("rpm implemented");
+		
+	}
+	public static void getcolor() {
+		System.out.println(color);
+	}
+
+}
---
+++ src/InheritancePractice/Parentclass.java
@@ -0,0 +1,38 @@
+package InheritancePractice;
+
+public class Parentclass {
+	 static String color = "red";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		gear();
+		engine();
+		
+		
+	}
+	public static void gear () {
+		System.out.println("gear method is implemented");
+	}
+	 
+	public static void engine() {
+		System.out.println("engine method is implemented in parent");
+	}
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
+
+
